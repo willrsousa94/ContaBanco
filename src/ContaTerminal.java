dac45acb250339466e213aab5cfbc07eb5bce123
William Sousa <williamrsousa94@hotmail.com>
2023-09-12 23:06:08 -0300

    Criação do programa que simula a criação da conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Qual o seu nome completo? ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int numConta = scanner.nextInt();
+
+        System.out.println("Quanto gostaria de depositar? ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+        + agencia + ", conta " + numConta + " e seu saldo R$" + saldo + " cjá está disponível para saque");
     }
 }
